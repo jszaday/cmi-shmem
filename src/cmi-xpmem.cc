cf604aa88c015a0d6d242c78ecaf3f8957685504
Justin Szaday <jjszaday@gmail.com>
2021-10-27 17:36:21 -0500

    Fix XPMEM Codepath

--- src/cmi-xpmem.cc
+++ src/cmi-xpmem.cc
@@ -42,7 +42,7 @@ struct ipc_metadata_ {
   // maps ranks to shared segments
   std::map<int, ipc_shared_*> shared;
   // maps ranks to (cached) blocks
-  // TODO ( the cache is NOT SMP safe )
+  // TODO ( the cache is _NOT_ SMP safe )
   // TODO ( take sizes into consideration )
   std::map<int, CmiIpcBlock*> cache;
   // our physical rank
@@ -62,16 +62,25 @@ struct ipc_metadata_ {
     CmiAssert(ins.second);
   }
 
+  CmiIpcBlock* get_cache(int rank) const {
+    auto search = this->cache.find(rank);
+    if (search == std::end(this->cache)) {
+      return (CmiIpcBlock*)kTail;
+    } else {
+      return search->second;
+    }
+  }
+
   CmiIpcBlock* seek_cached(int rank, std::size_t size) {
-    auto* block = this->cache[rank];
-    while (block != nullptr) {
+    auto* block = this->get_cache(rank);
+    while (block != (CmiIpcBlock*)kTail) {
       if (block->size >= size && block->free.load(std::memory_order_relaxed)) {
         break;
       } else {
         block = block->cached;
       }
     }
-    return block;
+    return (block == (CmiIpcBlock*)kTail) ? nullptr : block;
   }
 
   xpmem_segid_t get_segment(int rank) {
@@ -120,7 +129,7 @@ void* translateAddr_(ipc_metadata_* meta, int rank, void* remote_ptr,
   } else {
     auto apid = meta->get_instance(rank);
     CmiAssert(apid >= 0);
-
+    // this magic was borrowed from VADER
     uintptr_t attach_align = 1 << 23;
     auto base = OPAL_DOWN_ALIGN_PTR(remote_ptr, attach_align, uintptr_t);
     auto bound =
@@ -155,6 +164,7 @@ CmiIpcBlock* allocTranslateBlock_(ipc_metadata_* meta, int rank,
     CmiAssert(check == nullptr);
     return nullptr;
   } else {
+    // translate the "home" PE's address into a local one
     auto* xlatd = (CmiIpcBlock*)translateAddr_(meta, rank, head,
                                                size + sizeof(CmiIpcBlock));
     auto* next = xlatd->next;
@@ -174,12 +184,17 @@ CmiIpcBlock* CmiAllocBlock(int pe, std::size_t size) {
   auto* meta = CsvAccess(metadata_).get();
   auto* block = meta->seek_cached(theirRank, size);
   if (block == nullptr) {
+    // allocate a block from the rank
     block = allocTranslateBlock_(meta, theirRank, size);
+    // then acquire it if we succeed
     if (block != nullptr) {
       auto acq = acquireBlock_(meta, block);
       CmiAssert(acq);
     }
   } else {
+    // sanity check (we should get our own block)
+    CmiAssert(block->dst == meta->mine);
+    // set the block as in-use again
     auto free = block->free.exchange(false, std::memory_order_relaxed);
     CmiAssert(free);
   }
@@ -187,18 +202,20 @@ CmiIpcBlock* CmiAllocBlock(int pe, std::size_t size) {
 }
 
 // TODO ( implement this function! )
-// (OpenSHMEM has a mechanism for determining addr range?)
+// (OpenSHMEM might've had a mechanism for determining addr range?)
 CmiIpcBlock* CmiIsBlock(void* addr) {
   return (CmiIpcBlock*)((char*)addr - sizeof(CmiIpcBlock));
 }
 
 void CmiCacheBlock(CmiIpcBlock* block) {
   auto* meta = CsvAccess(metadata_).get();
-  CmiAssert(block->dst == meta->mine);
+  CmiAssert(meta->mine == block->dst);
+  // if the block hasn't been cached already:
   if (block->cached == nullptr) {
-    auto& cache = meta->cache[block->src];
-    block->cached = cache;
-    cache = block;
+    // add it to the cache linked list:
+    block->cached = meta->get_cache(block->src);
+    meta->cache[block->src] = block;
+    DEBUGP(("%d> cached block (%p) from %d.\n", meta->mine, block->orig, block->src)); 
   }
 }
 
@@ -214,7 +231,7 @@ void CmiFreeBlock(CmiIpcBlock* block) {
       pushBlock_(free, block);
     }
   } else {
-    // TODO ( implement this codepath )
+    // TODO ( implement this codepath! )
     CmiAbort("allocator-side free'ing unsupported\n");
   }
 }
@@ -229,6 +246,8 @@ bool CmiPushBlock(CmiIpcBlock* block) {
   auto* meta = CsvAccess(metadata_).get();
   CmiAssert(meta && (block->src != meta->mine));
   auto& queue = meta->shared[block->src]->queue;
+  // the value we want to save in the "home"
+  // queue is the block's original address
   return pushBlock_(queue, block, block->orig);
 }
 
@@ -246,11 +265,17 @@ CmiIpcBlock* CmiNewBlock(std::size_t size) {
 static void handleInitialize_(void* msg) {
   auto* imsg = (init_msg_*)msg;
   auto& meta = CsvAccess(metadata_);
+  // extract the segment id and shared region
+  // from the msg (registering it in our metadata)
   meta->put_segment(imsg->from, imsg->segid);
   meta->shared[imsg->from] = (ipc_shared_*)translateAddr_(
       meta.get(), imsg->from, imsg->shared, sizeof(ipc_shared_));
+  // then free the message
   CmiFree(imsg);
+  // if we received messages from all our peers:
   if (meta->nPeers == meta->shared.size()) {
+    // resume the sleeping thread
+    // TODO ( what if init hasn't been set yet? )
     CthResume(meta->init);
   }
 }
@@ -282,7 +307,6 @@ void CmiInitIpcMetadata(char** argv) {
   meta->nPeers = nPes;
 
   if (nPes > 1) {
-    // unsure whether this is allowed?
     meta->init = CthSelf();
     auto initHdl = CmiRegisterHandler(handleInitialize_);
     auto* imsg = (init_msg_*)CmiAlloc(sizeof(init_msg_));
@@ -303,10 +327,11 @@ void CmiInitIpcMetadata(char** argv) {
         // free'ing with the last send
         CmiSyncSendAndFree(pe, sizeof(init_msg_), (char*)imsg);
       } else {
-        // and just sending otherwise
+        // then sending (without free) otherwise
         CmiSyncSend(pe, sizeof(init_msg_), (char*)imsg);
       }
     }
+    // suspend until we receive messages from all our peers!
     CthSuspend();
   }
 }
